c166e369a6d5332761fe85c6d4bc4e5a71f2015f
salaros <salaros@yandex.ru>
2018-06-05 21:01:11 +0300

    cooler IndexOf and Contains for string enumerations

--- src/String.cs
+++ src/String.cs
@@ -1,6 +1,7 @@
 #region
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -94,8 +95,8 @@ namespace System
         /// <exception cref="ArgumentException"></exception>
         public static string ToSentenceCase(this string input)
         {
-            if (string.IsNullOrEmpty(input))
-                throw new ArgumentException();
+            if (string.IsNullOrEmpty(input)) throw new ArgumentException();
+
             return input.First().ToString().ToUpper() + input.Substring(1);
         }
 
@@ -108,8 +109,7 @@ namespace System
         public static string ToConstantCase(this string input)
         {
             const string separators = @"- []{}<>~`+=,.;:/?|!@#$%^&*()";
-            if (string.IsNullOrEmpty(input))
-                throw new ArgumentException();
+            if (string.IsNullOrEmpty(input)) throw new ArgumentException();
 
             input = separators.Aggregate(input, (current, separator) => current.Replace(separator, '_'));
             return input.ToUpperInvariant();
@@ -120,17 +120,75 @@ namespace System
         #region Contains
 
         /// <summary>
-        /// Determines whether [contains] [the specified needle].
+        /// Determines whether [haystack contains] [the specified needle].
         /// </summary>
         /// <param name="haystack">The haystack.</param>
         /// <param name="needle">The needle.</param>
-        /// <param name="options">The options.</param>
+        /// <param name="options">The compare options.</param>
+        /// <param name="culture">The culture.</param>
         /// <returns>
         ///   <c>true</c> if [contains] [the specified needle]; otherwise, <c>false</c>.
         /// </returns>
-        public static bool Contains(this string haystack, string needle, CompareOptions options)
+        public static bool Contains(this string haystack, string needle, CompareOptions options, CultureInfo culture = null)
         {
-            return CultureInfo.InvariantCulture.CompareInfo.IndexOf(haystack, needle, options) >= 0;
+            return (culture ?? CultureInfo.InvariantCulture).CompareInfo.IndexOf(haystack, needle, options) >= 0;
+        }
+
+        /// <summary>
+        /// Determines whether [haystack contains] [the specified needle].
+        /// </summary>
+        /// <param name="haystack">The haystack.</param>
+        /// <param name="needle">The needle.</param>
+        /// <param name="comparison">The comparer to use.</param>
+        /// <returns>
+        ///   <c>true</c> if [contains] [the specified needle]; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// haystack
+        /// or
+        /// needle
+        /// </exception>
+        public static bool Contains(this IEnumerable<string> haystack, string needle, StringComparison comparison)
+        {
+            if (null == haystack) throw new ArgumentNullException(nameof(haystack));
+            if (null == needle) throw new ArgumentNullException(nameof(needle));
+
+            return haystack.ToList().Any(str => needle.Equals(str, comparison));
+        }
+
+        /// <summary>
+        /// Returns the index of the first occurrence of value in the current instance.
+        /// The search starts at startIndex and runs thorough the next count characters.
+        /// </summary>
+        /// <param name="haystack">The haystack.</param>
+        /// <param name="needle">The needle.</param>
+        /// <param name="comparison">The comparer to use.</param>
+        /// <param name="startIndex">The starting index.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">
+        /// haystack
+        /// or
+        /// startIndex
+        /// </exception>
+        /// <exception cref="ArgumentException">needle</exception>
+        public static int IndexOf(this IEnumerable<string> haystack, string needle, StringComparison comparison, int startIndex = 0)
+        {
+            if (null == haystack) throw new ArgumentNullException(nameof(haystack));
+            if (null == needle) throw new ArgumentException(nameof(needle));
+
+            if (!(haystack is IList<string> enumerable))
+                return -1;
+
+            if (enumerable.Count <= startIndex)
+                throw new ArgumentNullException(nameof(startIndex));
+
+            for (var i = startIndex; i < enumerable.Count(); i++)
+            {
+                if (enumerable[i].Equals(needle, comparison))
+                    return i;
+            }
+
+            return -1;
         }
 
         #endregion Contains
